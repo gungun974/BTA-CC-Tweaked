3c8c44094f4311053de01b34bdc31ac4929fcbde
Gungun974 <xfelix974@gmail.com>
2025-04-13 22:02:09 +0400

    Make computer blink in inventory

--- src/main/java/dan200/computercraft/shared/computer/blocks/BlockModelComputer.java
+++ src/main/java/dan200/computercraft/shared/computer/blocks/BlockModelComputer.java
@@ -1,5 +1,6 @@
 package dan200.computercraft.shared.computer.blocks;
 
+import dan200.computercraft.ComputerCraft;
 import dan200.computercraft.shared.computer.core.ComputerState;
 import net.minecraft.client.render.block.model.BlockModelHorizontalRotation;
 import net.minecraft.client.render.texture.stitcher.IconCoordinate;
@@ -17,6 +18,31 @@ public class BlockModelComputer<T extends BlockLogic> extends BlockModelHorizont
 
     public IconCoordinate getBlockTexture(WorldSource blockAccess, int x, int y, int z, Side side) {
         int currentMetadata = blockAccess.getBlockMetadata(x, y, z);
+        int index = Sides.orientationLookUpHorizontal[6 * Math.min(currentMetadata & 7, 5) + side.getId()];
+        if (index >= Sides.orientationLookUpHorizontal.length) {
+            return this.blockTextures.get(Side.BOTTOM);
+        } else if (index == Side.NORTH.getId()) {
+            IconCoordinate originalFront = this.blockTextures.get(Side.NORTH);
+
+            final ComputerState currentState = ComputerState.class.getEnumConstants()[(currentMetadata >> 3) & 0b11];
+
+            ComputerCraft.log.info("hoi");
+
+            switch (currentState){
+                case ON:
+                    return TextureRegistry.getTexture(originalFront.namespaceId.namespace() + ":block/" + originalFront.namespaceId.value() + "_on");
+                case BLINKING:
+                    return TextureRegistry.getTexture(originalFront.namespaceId.namespace() + ":block/" + originalFront.namespaceId.value() + "_blink");
+                case OFF:
+                default:
+                    return originalFront;
+            }
+        } else {
+            return this.blockTextures.get(Side.getSideById(index));
+        }
+    }
+
+    public IconCoordinate getBlockTextureFromSideAndMetadata(Side side, int currentMetadata) {
         int index = Sides.orientationLookUpHorizontal[6 * Math.min(currentMetadata & 7, 5) + side.getId()];
         if (index >= Sides.orientationLookUpHorizontal.length) {
             return this.blockTextures.get(Side.BOTTOM);
