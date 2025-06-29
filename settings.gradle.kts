c0ae2549d438ac1656b7e7ae2a6db676ec857582
Apollointhehouse <apollocameronboot@gmail.com>
2025-06-29 20:44:18 +1200

    Switched from groovy to kotlin build DSL and fixed checkVersion in build script checking for wrong version

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        maven {
+            name = "Jitpack"
+            url = uri("https://jitpack.io")
+        }
+        maven {
+            name = "Babric"
+            url = uri("https://maven.glass-launcher.net/babric")
+        }
+        maven {
+            name = "SignalumMavenInfrastructure"
+            url = uri("https://maven.thesignalumproject.net/infrastructure")
+        }
+    }
+}
+
+rootProject.name = "bta-cc"
