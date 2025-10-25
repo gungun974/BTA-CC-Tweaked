8d3221a59e30e4db7f2a4f77696f6fe2169adc33
Gungun974 <xfelix974@gmail.com>
2025-10-25 21:30:30 +0400

    Replace `WorldClientMixin` with `WorldMixin` for better compatibility with mods

--- src/main/java/dan200/computercraft/fabric/mixin/WorldMixin.java
+++ src/main/java/dan200/computercraft/fabric/mixin/WorldMixin.java
@@ -1,7 +1,12 @@
 package dan200.computercraft.fabric.mixin;
 
+import dan200.computercraft.ComputerCraft;
+import dan200.computercraft.client.FrameInfo;
+import dan200.computercraft.core.computer.MainThread;
 import dan200.computercraft.shared.common.ComputerCraftBlocks;
 import dan200.computercraft.shared.util.DropConsumer;
+import dan200.computercraft.shared.util.PortableTickScheduler;
+import dan200.computercraft.shared.util.TickScheduler;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.Blocks;
 import net.minecraft.core.entity.Entity;
@@ -12,7 +17,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import turniplabs.halplibe.helper.EnvironmentHelper;
 
 @Mixin(value = World.class, remap = false)
 public abstract class WorldMixin {
@@ -20,6 +27,28 @@ public abstract class WorldMixin {
     public @Nullable Block<?> getBlock(int x, int y, int z) {
         return null;
     }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    public void tickStart(CallbackInfo ci) {
+        if (!EnvironmentHelper.isSinglePlayer()) {
+            return;
+        }
+        PortableTickScheduler.mainPortableTickScheduler.tickAtStart();
+        MainThread.executePendingTasks();
+        ComputerCraft.serverComputerRegistry.update();
+        TickScheduler.tick();
+        FrameInfo.onTick();
+        PortableTickScheduler.mainPortableTickScheduler.tickAtEnd();
+    }
+
+    @Inject(method = "tick", at = @At("RETURN"))
+    public void tickEnd(CallbackInfo ci) {
+        if (!EnvironmentHelper.isSinglePlayer()) {
+            return;
+        }
+        PortableTickScheduler.mainPortableTickScheduler.tickAtEnd();
+    }
+
 
     @Inject(method = "getSignal(IIILnet/minecraft/core/util/helper/Side;)Z", at = @At("HEAD"), cancellable = true)
     public void computerSignal(int x, int y, int z, Side side, CallbackInfoReturnable<Boolean> cir) {
