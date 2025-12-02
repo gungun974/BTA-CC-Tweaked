0b69db851d790c4ff3233762dfe644292326a6ac
Gungun974 <xfelix974@gmail.com>
2025-12-02 23:35:39 +0400

    Use Halplibe EnvironmentHelper

--- src/main/java/dan200/computercraft/fabric/Helper.java
+++ src/main/java/dan200/computercraft/fabric/Helper.java
@@ -1,28 +1,23 @@
 package dan200.computercraft.fabric;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.Global;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.BlockLogic;
 import net.minecraft.core.world.World;
+import turniplabs.halplibe.helper.EnvironmentHelper;
 
 import javax.annotation.Nullable;
 
 public class Helper {
     public static boolean isServerEnvironment() {
-        return Global.isServer;
+        return EnvironmentHelper.isServerEnvironment();
     }
 
     public static boolean isSinglePlayer() {
-        if (Global.isServer) {
-            return false;
-        }
-
-        return !Minecraft.getMinecraft().isMultiplayerWorld();
+        return EnvironmentHelper.isSinglePlayer();
     }
 
     public static boolean isClientWorld() {
-        return !isSinglePlayer() && !isServerEnvironment();
+        return EnvironmentHelper.isClientWorld();
     }
 
     public static @Nullable BlockLogic getBlockLogic(final World world, final int x, final int y, final int z) {
