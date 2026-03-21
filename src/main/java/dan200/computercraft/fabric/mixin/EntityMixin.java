01ba1103fd9318d73e2ec78aed7f1f41c18ccb6a
Gungun974 <xfelix974@gmail.com>
2026-03-21 19:42:51 +0400

    Prevent game crash when turtle attack and try to intercept a dropStack

--- src/main/java/dan200/computercraft/fabric/mixin/EntityMixin.java
+++ src/main/java/dan200/computercraft/fabric/mixin/EntityMixin.java
@@ -5,12 +5,22 @@ import net.minecraft.core.entity.Entity;
 import net.minecraft.core.entity.EntityItem;
 import net.minecraft.core.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Entity.class, remap = false)
 public class EntityMixin {
+    @Shadow
+    public double z;
+
+    @Shadow
+    public double y;
+
+    @Shadow
+    public double x;
+
     @Inject(
         method = "dropItem(Lnet/minecraft/core/item/ItemStack;F)Lnet/minecraft/core/entity/EntityItem;",
         at = @At("HEAD"),
@@ -18,7 +28,10 @@ public class EntityMixin {
     )
     private void dropStack(ItemStack stack, float verticalOffset, CallbackInfoReturnable<EntityItem> cir) {
         if (DropConsumer.onLivingDrops((Entity) (Object) this, stack)) {
-            cir.setReturnValue(null);
+            EntityItem entityitem = new EntityItem(null, x, y + (double)verticalOffset, z, stack);
+
+            cir.cancel();
+            cir.setReturnValue(entityitem);
         }
     }
 }
