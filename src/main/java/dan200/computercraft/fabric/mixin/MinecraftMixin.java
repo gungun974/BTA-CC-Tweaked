de906c2320cb8397688a9aff79d901165f105397
Gungun974 <xfelix974@gmail.com>
2026-08-27 19:49:20 +0400

    Fix new world dir name on single player was null

--- src/main/java/dan200/computercraft/fabric/mixin/MinecraftMixin.java
+++ src/main/java/dan200/computercraft/fabric/mixin/MinecraftMixin.java
@@ -2,22 +2,38 @@ package dan200.computercraft.fabric.mixin;
 
 import dan200.computercraft.fabric.IWorldDirNameAccess;
 import net.minecraft.client.Minecraft;
+import net.minecraft.core.world.save.ISaveFormat;
+import net.minecraft.core.world.settings.WorldConfiguration;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.File;
+
 @Mixin(value = Minecraft.class, remap = false)
 public abstract class MinecraftMixin implements IWorldDirNameAccess {
+    @Shadow
+
+    private ISaveFormat saveFormat;
     @Unique
     public String worldDirName;
 
-    @Inject(
-        method = "startWorld(Ljava/lang/String;)V",
-        at = @At("HEAD")
-    )
-    private void rememberWorldDirName(String worldDirName, CallbackInfo ci) {
+    @Inject(method = "createAndStartWorld", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/client/Minecraft;changeWorld(Lnet/minecraft/client/world/WorldClient;Ljava/lang/String;)V"
+    ))
+    public void rememberNewWorldDirName(WorldConfiguration worldConfiguration, CallbackInfo ci) {
+        this.worldDirName = worldConfiguration.getFolderName(this.saveFormat);
+    }
+
+    @Inject(method = "startWorld", at = @At(
+        value = "INVOKE",
+        target = "Lnet/minecraft/client/Minecraft;changeWorld(Lnet/minecraft/client/world/WorldClient;Ljava/lang/String;)V"
+    ))
+    public void rememberWorldDirName(String worldDirName, CallbackInfo ci) {
         this.worldDirName = worldDirName;
     }
 
